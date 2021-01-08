d8cc07ea095e6a9e9833c3629e5058e56e5bb61f
miguelhun <hunmiguel1124@gmail.com>
2021-01-08 12:30:31 -0500

    defined tokens

---
+++ lexer/token.go
@@ -0,0 +1,68 @@
+package lexer
+
+type tokenType int
+
+const (
+	ILLEGAL tokenType = iota
+	EOF
+
+	// data types
+	IDENT
+	INT
+	DOUBLE
+	STRING
+	BOOL
+
+	// arithmetic operators
+	PLUS
+	MINUS
+	ASTERISK
+	SLASH
+	MODULO
+
+	// assignment operators
+	EQUAL
+	PLUS_EQUAL
+	MINUS_EQUAL
+	ASTERISK_EQUAL
+	SLASH_EQUAL
+	NOT
+
+	// comparison operators
+	EQUAL_TO
+	NOT_EQUAL
+	LESS
+	GREATER
+	LESS_EQUAL
+	GREATER_EQUAL
+	AND
+	OR
+
+	// delimiters
+	COMMA
+	SEMICOLON
+	COLON
+	LPAREN
+	RPAREN
+	LBRACKET
+	RBRACKET
+	LBRACE
+	RBRACE
+
+	// keywords
+	LET
+	FUNC
+	TRUE
+	FALSE
+	IF
+	ELSE
+	FOR
+	WHILE
+	RETURN
+	TYPE
+)
+
+type Token struct {
+	Type  tokenType
+	value string
+}
