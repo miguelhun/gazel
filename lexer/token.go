68efdacf35195c117801df453e9c88f4c5ee53d7
miguelhun <hunmiguel1124@gmail.com>
2021-02-04 16:16:25 -0600

    change to tokens

--- lexer/token.go
+++ lexer/token.go
@@ -3,13 +3,13 @@ package lexer
 type tokenType int
 
 const (
-	ILLEGAL tokenType = iota
+	ILLEGAL = iota
 	EOF
 
 	// data types
 	IDENT
 	INT
-	DOUBLE
+	//	DOUBLE
 	STRING
 	BOOL
 
@@ -61,6 +61,67 @@ const (
 	RETURN
 	TYPE
 )
+
+var tokens = [...]string{
+	EOF:     "EOF",
+	ILLEGAL: "ILLEGAL",
+
+	// data types
+	IDENT: "IDENT",
+	INT:   "INT",
+	//	DOUBLE: "DOUBLE",
+	STRING: "STRING",
+	BOOL:   "BOOL",
+
+	// arithmetic operators
+	PLUS:     "+",
+	MINUS:    "-",
+	ASTERISK: "*",
+	SLASH:    "/",
+	MODULO:   "%",
+
+	// assignment operators
+	EQUAL:          "=",
+	PLUS_EQUAL:     "+=",
+	MINUS_EQUAL:    "-=",
+	ASTERISK_EQUAL: "*=",
+	SLASH_EQUAL:    "/=",
+	NOT:            "!",
+
+	// comparison operators
+	EQUAL_TO:      "==",
+	NOT_EQUAL:     "!=",
+	LESS:          "<",
+	GREATER:       ">",
+	LESS_EQUAL:    "<=",
+	GREATER_EQUAL: ">=",
+	AND:           "&&",
+	OR:            "||",
+
+	// delimiters
+	COMMA:     ",",
+	SEMICOLON: ";",
+	COLON:     ":",
+	LPAREN:    "(",
+	RPAREN:    ")",
+	LBRACKET:  "[",
+	RBRACKET:  "]",
+	LBRACE:    "{",
+	RBRACE:    "}",
+
+	// keywords
+	LET:    "LET",
+	FUNC:   "FUNC",
+	TRUE:   "TRUE",
+	FALSE:  "FALSE",
+	IF:     "IF",
+	ELSE:   "ELSE",
+	FOR:    "FOR",
+	WHILE:  "WHILE",
+	RETURN: "RETURN",
+	//TYPE: "TYPE",
+
+}
 
 type Token struct {
 	tokType tokenType
